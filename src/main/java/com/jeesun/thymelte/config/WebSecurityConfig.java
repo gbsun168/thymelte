20d47badbbf36da74dbf6361383532cd6f6c0753
SimonSun <simon.sun.dev@hotmail.com>
2018-03-31 16:44:36 +0800

    配置Spring Security，连接数据库验证用户登录信息。

---
+++ src/main/java/com/jeesun/thymelte/config/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.jeesun.thymelte.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DruidDataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .formLogin().loginPage("/login").permitAll()
+                .defaultSuccessUrl("/index")
+                .failureUrl("/login?error").permitAll()
+                .and()
+                .logout().permitAll()
+                .and().authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/img/**", "/js/**", "/css/**", "/webjars/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .rememberMe();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("select username,password,enabled from users where username = ?")
+                .authoritiesByUsernameQuery("select username,authority from authorities where username = ?")
+                .passwordEncoder(new BCryptPasswordEncoder(11));
+    }
+
+    /**
+     * 使Thymeleaf的spring security标签生效
+     * 使用办法：html标签内添加xmlns:sec="http://www.thymeleaf.org/thymeleaf-extras-springsecurity4"
+     * @return
+     */
+    @Bean
+    public SpringSecurityDialect securityDialect() {
+        return new SpringSecurityDialect();
+    }
+}
