5c6809de06d032b50369b8e6d581550bc77fa1ad
SimonSun <simon.sun.dev@hotmail.com>
2018-04-04 00:55:22 +0800

    统计在线人数。

--- src/main/java/com/jeesun/thymelte/controller/UserController.java
+++ src/main/java/com/jeesun/thymelte/controller/UserController.java
@@ -6,9 +6,11 @@ import org.springframework.security.web.authentication.logout.SecurityContextLog
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class UserController {
@@ -25,6 +27,14 @@ public class UserController {
         if (auth != null){
             new SecurityContextLogoutHandler().logout(request, response, auth);
         }
-        return "redirect:/login?logout";
+        return "redirect:/login";
+    }
+
+    @RequestMapping("/count")
+    @ResponseBody
+    public String count(HttpServletRequest request, HttpServletResponse response){
+        HttpSession session = request.getSession();
+        Object count = session.getServletContext().getAttribute("count");
+        return "count : " + count;
     }
 }
