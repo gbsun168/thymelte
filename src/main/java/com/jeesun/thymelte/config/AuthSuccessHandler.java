50a1b82eaf8fe8dbde9b3cba1444ba4410135ac9
SimonSun <simon.sun.dev@hotmail.com>
2018-06-10 11:28:51 +0800

    日志打印session创建时间。

--- src/main/java/com/jeesun/thymelte/config/AuthSuccessHandler.java
+++ src/main/java/com/jeesun/thymelte/config/AuthSuccessHandler.java
@@ -1,26 +1,27 @@
 package com.jeesun.thymelte.config;
 
+import com.alibaba.fastjson.JSON;
 import com.jeesun.thymelte.domain.LogLogin;
 import com.jeesun.thymelte.domain.UserEntity;
 import com.jeesun.thymelte.repository.LogLoginRepository;
+import com.jeesun.thymelte.util.DateUtil;
 import com.jeesun.thymelte.util.IpUtil;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Component
 public class AuthSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
-    private static Logger log = Logger.getLogger(AuthSuccessHandler.class);
+    private static Logger logger = Logger.getLogger(AuthSuccessHandler.class);
 
     @Autowired
     private LogLoginRepository logLoginRepository;
@@ -28,7 +29,12 @@ public class AuthSuccessHandler extends SavedRequestAwareAuthenticationSuccessHa
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         UserEntity userDetails = (UserEntity) authentication.getPrincipal();
-        logLoginRepository.save(new LogLogin(userDetails.getUsername(), new Date(), IpUtil.getIpAddr(request)));
+        LogLogin logLogin = new LogLogin(userDetails.getUsername(), new Date(), IpUtil.getIpAddr(request));
+        logLogin = logLoginRepository.save(logLogin);
+        logger.info(JSON.toJSONString(logLogin));
+
+        HttpSession httpSession = request.getSession();
+        logger.info("session create time = " + DateUtil.format(httpSession.getCreationTime(), "yyyy-MM-dd HH:mm:ss"));
 
         super.onAuthenticationSuccess(request, response, authentication);
     }
