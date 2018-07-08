d7ece5a8ede4fff7dc6dfd1b6baa6fca8cf74dc6
SimonSun <simon.sun.dev@hotmail.com>
2018-07-08 12:39:20 +0800

    自定义buttonLoader插件。

--- src/main/resources/static/plug-in/buttonLoader/jquery/plugin/jquery.buttonLoader.js
+++ src/main/resources/static/plug-in/buttonLoader/jquery/plugin/jquery.buttonLoader.js
@@ -4,26 +4,37 @@
 */
 (function ($) {
     $('.has-spinner').attr("disabled", false);
-    $.fn.buttonLoader = function (action) {
+    $.fn.buttonLoader = function (action, message) {
         var self = $(this);
-        if (action == 'start') {
+        if (action == 'loading') {
             if ($(self).attr("disabled") == "disabled") {
                 return false;
             }
             $('.has-spinner').attr("disabled", true);
             $(self).attr('data-btn-text', $(self).text());
-            var text = 'Loading';
+            var text = message;
             console.log($(self).attr('data-load-text'));
             if($(self).attr('data-load-text') != undefined && $(self).attr('data-load-text') != ""){
-                var text = $(self).attr('data-load-text');
+                text = $(self).attr('data-load-text');
             }
             $(self).html('<span class="spinner"><i class="fa fa-spinner fa-spin" title="button-loader"></i></span> '+text);
             $(self).addClass('active');
         }
-        if (action == 'stop') {
-            $(self).html($(self).attr('data-btn-text'));
+
+        if (action == 'success') {
+            $(self).html('<i class="fa fa-check" aria-hidden="true"></i> ' + message);
             $(self).removeClass('active');
             $('.has-spinner').attr("disabled", false);
+            $('.has-spinner').removeClass('btn-default btn-primary');
+            $('.has-spinner').addClass('btn-success');
+        }
+
+        if (action == 'error') {
+            $(self).html('<i class="fa fa-close" aria-hidden="true"></i> ' + message);
+            $(self).removeClass('active');
+            $('.has-spinner').attr("disabled", false);
+            $('.has-spinner').removeClass('btn-default btn-primary');
+            $('.has-spinner').addClass('btn-danger');
         }
     }
 })(jQuery);
